0a8b80fe14c80769812d5fbb23a605726aa20a62
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-14 20:57:37 +0200

    work on nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
+import NavLogo from '@/components/nav/NavLogo';
 import NavLinks from '@/components/nav/NavLinks';
 import SocialMediaIcons from '@/components/ui/SocialMediaIcons';
+import Hamburger from '@/components/nav/Hamburger';
+
+const navLinks = [{ name: 'Home' }, { name: 'About' }, { name: 'Tracks' }];
 
 export default function Nav() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  function toggleNavbarHandler() {
+    setIsNavbarOpen(!isNavbarOpen);
+  }
+
+  function closeNavbarHandler() {
+    setIsNavbarOpen(false);
+  }
+
   return (
-    <header>
-      <nav className='container mx-auto bg-red-500 flex justify-between items-center'>
-        <p>Dj Adrian Andrea</p>
-        <NavLinks />
-        <SocialMediaIcons />
-      </nav>
-    </header>
+    <>
+      <header className='container mx-auto w-full py-4 lg:py-0'>
+        <nav className='relative flex flex-wrap items-center justify-between'>
+          <NavLogo />
+          <Hamburger isNavbarOpen={isNavbarOpen} onToggleNavbar={toggleNavbarHandler} />
+          <div
+            className={`items-center  lg:flex
+           ${isNavbarOpen ? ' flex' : ' hidden'}`}
+          >
+            <ul className='fixed left-0 top-0  z-30 flex h-screen w-full flex-col items-center justify-center gap-10 bg-red-800 px-2 py-10 lg:static lg:ml-auto lg:h-auto lg:w-auto lg:flex-row lg:gap-0 lg:bg-transparent'>
+              {navLinks.map((link) => (
+                <NavLinks
+                  link={link}
+                  key={link.name}
+                  onCloseNavbar={closeNavbarHandler}
+                />
+              ))}
+            </ul>
+          </div>
+          <SocialMediaIcons />
+        </nav>
+      </header>
+    </>
   );
 }
