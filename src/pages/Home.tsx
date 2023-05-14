a76ece0cbb18d15cdef911a712d215fa391f2b6d
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-14 21:58:53 +0200

    work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Nav from '@/components/nav/Nav';
+import HeroSection from '@/components/sections/HeroSection';
 import '../index.css';
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <HeroSection />
     </>
   );
 }
